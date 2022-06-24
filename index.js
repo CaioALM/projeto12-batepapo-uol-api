f29e6d0e9f9ca945060c40dc79de46e84da8252f
Caio <caiosobreira77@gmail.com>
2022-06-24 11:16:45 -0300

    add libraries

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-import chalk from 'chalk'
+import { MongoClient, ObjectId } from 'mongodb'
 import express, { json } from 'express'
+import chalk from 'chalk'
 import cors from 'cors'
+import dayjs from 'dayjs'
 import dotenv from 'dotenv'
+import joi from 'joi'
 
 dotenv.config()
 
 const app = express()
 
+
 app.use(cors())
 app.use(json())
 
 app.post('/', (req, res) => {
-console.log(chalk.red.bold("Está entrando no post"))
+
+
 })
 
 app.get('/', (req, res) => {
-    console.log(chalk.blue.bold("Está entrando no get"))
+
 })
 
-app.listen(PORT, () => {
+app.listen(5000, () => {
     console.log(chalk.bold.green('Server Online'))
 })
